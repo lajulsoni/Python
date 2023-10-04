036ce39c7cff090cffd920e55a6aa619d17d3f53
lajulsoni <lajulsoni>
2023-10-04 15:05:26 +0530

    logic change

--- prime.py
+++ prime.py
@@ -1,12 +1,13 @@
+from math import sqrt
 num=int(input("Enter a number: "))  
       
 if num > 1:  
-    for i in range(2,num):  
+    for i in range(2,(sqrt(num)+1)):  
         if (num % i) == 0:  
              print(num,"is not a prime number")  
              break  
-        else:  
-             print(num,"is a prime number") 
-             break
+    else:  
+        print(num,"is a prime number") 
+
 else:  
     print(num,"is not a prime number")  
